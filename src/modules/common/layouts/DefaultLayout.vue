661fd34fdc7ef3987051767d28ce096bf3b91aa3
FrancescQuintoAnguera <francescquinto@iesmontsia.org>
2026-02-12 20:32:23 +0100

    Added the dependenties

--- src/modules/common/layouts/DefaultLayout.vue
+++ src/modules/common/layouts/DefaultLayout.vue
@@ -1,15 +1,8 @@
 <template>
-    <div class="bg-white">
-        <NavBar />
-        <SideBar />
-        <main class="flex justify-center transition-all duration-300 pt-20 pl-16 md:pl-[180px] md:mt-15 p-2 md:p-6">
-            <RouterView />
-        </main>
-    </div>
+     <RouterView />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import NavBar from '@/modules/common/components/NavBar.vue';
-import SideBar from '@/modules/common/components/SideBar.vue';
+
 </script>
