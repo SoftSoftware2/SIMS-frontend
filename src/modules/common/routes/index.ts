29e36fa679e8f24d587b41e6d12863749e7f8806
Iker Vericat <ikervericat@iesmontsia.org>
2026-02-16 20:23:01 +0100

    Merge develop into feature/39

--- src/modules/common/routes/index.ts
+++ src/modules/common/routes/index.ts
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import type { RouteRecordRaw } from "vue-router";
-import homeRoutes from "@/modules/home/routes";
-import companiesRoutes from "@/modules/companies/routes";
-import vehicleTypesRoutes from "@/modules/vehicle-types/routes";
-import vehiclesRoutes from "@/modules/vehicles/routes";
-
-const privateRoutes : RouteRecordRaw[] = [
-    {
-        path: '/app',
-        name: 'public',
-        component: ()=> import('@/modules/common/layouts/DefaultLayout.vue'),
-        children:[
-            ... homeRoutes,
-            ... companiesRoutes,
-            ... vehicleTypesRoutes,
-            ... vehiclesRoutes,
-        ]
-    }
-];
-
-export default privateRoutes;
-=======
 import type { RouteRecordRaw } from "vue-router";
 import homeRoutes from "@/modules/home/routes";
 import companiesRoutes from "@/modules/companies/routes";
@@ -40,4 +17,3 @@ const privateRoutes : RouteRecordRaw[] = [
 ];
 
 export default privateRoutes;
->>>>>>> ce2c756203c1f21dc64eddaf0ce60e5c439b2cfb
